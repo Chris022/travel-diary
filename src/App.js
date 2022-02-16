56a5c85cbf2555f2495d53a4627c3b5770c0675b
unknown <christoph02.cw@gmail.com>
2022-02-16 22:11:30 +0100

    :construction: remove external css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 import markerIconPng from "leaflet/dist/images/marker-icon.png"
 import {Icon} from 'leaflet'
 import 'leaflet/dist/leaflet.css'
-import "./App.css"
+
 
 function App() {
   const position = [51.505, -0.09]
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} style={{"height":"100vh"}}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
